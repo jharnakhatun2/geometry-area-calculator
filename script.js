82f4f55dad77dda1fb46aa41b4563ac1fddcab23
Jharna Khatun <jharnakhatun203@gmail.com>
2024-11-06 21:36:42 +0600

    input value set and show in edit item for Triangle

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 // get id
 const getId = (id) => document.getElementById(id);
 
+//create element
+const createElement = (tag) => document.createElement(tag);
+
 // add Event Listener
 const addEventListener = (element, event, callbackFn) =>
   element.addEventListener(event, callbackFn);
@@ -22,31 +25,66 @@ const getInputFieldValue = (id) => {
 };
 
 // Generalized area calculation
-const calculateArea = (arrayOfIds,areaFormula,displayElement) =>{
-const input = arrayOfIds.map(id => getInputFieldValue(id));
-const area = areaFormula(...input);
-result(displayElement, area);
+const calculateArea = (arrayOfIds, areaFormula, displayElement) => {
+  const input = arrayOfIds.map((id) => getInputFieldValue(id));
+  const area = areaFormula(...input);
+  result(displayElement, area);
 
-// reset input field
-arrayOfIds.forEach( id => getId(id).value = ''); 
-}
-  
-  // Triangle area calculation using the utility function
-  const triangleBtn = getId('triangle');
-  const triangleResult = getId('triangleResult');
-  const triangleAreaFormula = (base, height) => base * height * 0.5 ;
-  addEventListener(triangleBtn, 'click', ()=>{
-    calculateArea(['tBase','tHight'], triangleAreaFormula, triangleResult)
-  });
-  
-
-  // Rectangle area calculation using the utility function
-  const rectangleBtn = getId('rectangle');
-  const rectangleFormula = (width, length) => width * length ;
-  const rectangleResult = getId('rectangleResult')
-  addEventListener(rectangleBtn, 'click', ()=>{
-    calculateArea(['rWidth','rLength'], rectangleFormula, rectangleResult)
-  })
+  // reset input field
+  arrayOfIds.forEach((id) => (getId(id).value = ""));
+};
 
 
 
+//input value set and show in edit item for Triangle
+// select elements
+const baseD = getId("baseD");
+const heightD = getId("heightD");
+const checkbox = getId("checkbox");
+const editIcon = getId("editIcon");
+
+// add eventListener in checkbox
+addEventListener(checkbox, "click", () => {
+  const tBase = getInputFieldValue("tBase");
+  const tHight = getInputFieldValue("tHight");
+  baseD.textContent = tBase;
+  heightD.textContent = tHight;
+  getId("tBase").value = "";
+  getId("tHight").value = "";
+});
+addEventListener(editIcon, "click", () => {
+  // create edit input elements
+  const inputB = createElement('input');
+  const inputH = createElement('input');
+  // set inner value
+  inputB.value = baseD.innerHTML;
+  inputH.value = heightD.innerHTML;
+  inputB.classList.add('w-8', 'text-center');
+  inputH.classList.add('w-8', 'text-center');
+
+  // clear existing element and set new value
+  baseD.innerHTML = '';
+  heightD.innerHTML = '';
+  baseD.appendChild(inputB);
+  heightD.appendChild(inputH);
+
+  // update 
+  addEventListener(inputB, 'blur', ()=> baseD.innerHTML = inputB.value );
+  addEventListener(inputH, 'blur', ()=> heightD.innerHTML = inputH.value );
+})
+
+// Triangle area calculation using the utility function
+const triangleBtn = getId("triangle");
+const triangleResult = getId("triangleResult");
+const triangleAreaFormula = (base, height) => base * height * 0.5;
+addEventListener(triangleBtn, "click", () => {
+  calculateArea(["tBase", "tHight"], triangleAreaFormula, triangleResult);
+});
+
+// Rectangle area calculation using the utility function
+const rectangleBtn = getId("rectangle");
+const rectangleFormula = (width, length) => width * length;
+const rectangleResult = getId("rectangleResult");
+addEventListener(rectangleBtn, "click", () => {
+  calculateArea(["rWidth", "rLength"], rectangleFormula, rectangleResult);
+});
